8656e1284974b5db94b80884d6c3d62c4ae26c70
chanley <chanley@stsci.edu>
2004-06-28 18:03:42 +0000

    06/28/04 -- CJH -- Initial check-in.  Previous CVS history for these modules can be found under 'mdrizzle/pytools/src'.
    git-svn-id: http://svn.stsci.edu/svn/ssb/stsci_python/stsci_python/trunk/imagestats@27 fe389314-cf27-0410-b35b-8c050e845b92

---
+++ src/buildHistogram.c
@@ -0,0 +1,104 @@
+/*
+    Program:    buildHistogram.c
+    Author:     Christopher Hanley
+    Purpose:    Populate a 1 dimensional python object to create a histogram
+
+    Version:
+            Version 0.1.0, 15-Jan-2004: Created -- CJH
+            Version 0.1.1, 26-Feb-2004: Bug fix.  Py_DECREF() was not being 
+                                        called for histogram.
+            Version 0.1.2, 26-May-2004: Bug Fix.  When computing the index the
+                difference needed to be case as type double to prevent loss of
+                precision.
+            Version 0.1.3, 27-May-2004: Removed Pydecref statments to avoid
+                pointer errors.  -- CJH
+            Version 0.1.4, 07-Jun-2004: Put Pydecref statements back into to plug 
+                memory leak, corrected boundary conditions on clipping loop.
+            Version 0.1.5, 21-Jun-2004: Added code to check that the population of
+                histogram at boundaries is not corrupted by 
+ 
+
+*/
+#include <Python.h>
+#include <arrayobject.h>
+#include <string.h>
+#include <numarray.h>
+#include <stdio.h>
+
+int populate1DHist_(float *image, int image_elements, 
+		    unsigned int *histogram, int histogram_elements,
+                    float minValue, float maxValue, float binWidth)
+{
+    int i, index=0;
+    for (i = 0; i < image_elements; i++) {
+        if ( (image[i] >= minValue) && (image[i] < maxValue) ) {
+            index = (int)((double)(image[i] - minValue) / binWidth );
+
+            /* Check the boundary conditions so that index of histogram remain is valid range 
+	        assert((index >= 0) && (index < histogram_elements));
+            if (!((index >= 0) && (index < histogram_elements))) {
+              fprintf(stderr, "index: %d, image[%d]: %f, minValue: %f, maxValue: %f\n", 
+                      index, i, image[i], minValue, maxValue);
+              fprintf(stderr, "binWidth: %f, histogram_elements: %d\n",
+                      binWidth, histogram_elements);
+            }
+            */
+
+            /* Handle histogram population for floating point errors at end points */
+            /* Case 1: Populating below index 0.*/
+            if ( index < 0 ) {
+                /*fprintf(stderr,"ERROR: trying to populate a negative index!\n");*/
+                histogram[0] += 1;
+            }
+            /* Case 2: Populating above the maximum index value*/
+            else if (index >= histogram_elements ) {
+                /*fprintf(stderr,"ERROR: trying to populate past end of histogram!\n");*/
+                histogram[histogram_elements - 1] +=1;
+            }
+            /* Case 3: Normal Case - Population of histogram occurs as expected in valid index range */
+            else {
+                histogram[ index ] += 1;    
+            }
+        } 
+    }
+    return 1;
+}
+
+static PyObject * populate1DHist(PyObject *obj, PyObject *args)
+{
+    PyObject *oimage, *ohistogram;
+    PyArrayObject *image, *histogram;
+    float minValue, maxValue, binWidth;
+    int status=0;
+
+    if (!PyArg_ParseTuple(args,"OOfff:populate1DHist",&oimage,&ohistogram,&minValue,&maxValue,&binWidth))
+	    return NULL;
+
+    image = (PyArrayObject *)NA_InputArray(oimage, tFloat32, C_ARRAY);
+    if (!image) return NULL;
+
+    histogram = (PyArrayObject *)NA_IoArray(ohistogram, tUInt32, C_ARRAY);
+    if (!histogram) return NULL;
+    
+    status = populate1DHist_(NA_OFFSETDATA(image), NA_elements(image),
+			     NA_OFFSETDATA(histogram), NA_elements(histogram),
+			     minValue, maxValue, binWidth);
+
+    Py_XDECREF(image);
+    Py_XDECREF(histogram);
+
+    return Py_BuildValue("i",status);
+}
+
+static PyMethodDef buildHistogram_methods[] =
+{
+    {"populate1DHist",  populate1DHist, METH_VARARGS, 
+        "populate1Dhist(image, histogram, minValue, maxValue, binWidth)"},
+    {0,            0}                             /* sentinel */
+};
+
+void initbuildHistogram(void) {
+    Py_InitModule("buildHistogram", buildHistogram_methods);
+    import_array();
+}
+
---
+++ src/computeMean.c
@@ -0,0 +1,111 @@
+/*
+    Program:    computeMean.c
+    Author:     Christopher Hanley
+    Purpose:    Compute the mean, stddev, max, and min for a nnumarray object
+                while applying some upper and lower pixel clipping values.
+
+    Version:
+            Version 0.1.0, 23-Feb-2004: Created -- CJH
+            Version 0.1.1, 24-May-2004: Made sum, sumsq type double to prevent 
+                register overflow -- CJH  
+            Version 0.1.2, 27-May-2004: Removed Pydecref statments to avoid
+                pointer errors.  -- CJH
+            Version 0.1.3, 07-Jun-2004, Implemented J. Miller's cleaned up
+                version of my code.  --CJH
+*/
+#include <Python.h>
+#include <arrayobject.h>
+#include <string.h>
+#include <numarray.h>
+#include <stdio.h>
+#include <math.h>
+
+int computeMean_(float *image, int nelements, float clipmin, float clipmax, 
+                               int *numGoodPixels, float *mean, float *stddev, 
+                                float *minValue, float *maxValue)
+{
+    int i;
+    float tmpMinValue, tmpMaxValue;
+    double sum, sumsq;
+
+    /* Initialize some local variables */
+    sum = 0;
+    sumsq = 0;
+
+    /*Initialize the tmpMinValue and tmpMaxValue so that we can find the 
+      largest and smallest non-clipped values */
+
+    tmpMinValue = clipmax;
+    tmpMaxValue = clipmin;
+
+    for (i = 0; i < nelements; i++) {
+        if ( (image[i] >= clipmin) && (image[i] <= clipmax) ) {
+            /* Find lowest value in the clipped image */
+            if (image[i] < tmpMinValue) {
+                tmpMinValue = image[i];
+            }
+
+            /* Find largest value in the clipped image */
+            if (image[i] > tmpMaxValue) {
+                tmpMaxValue = image[i];
+            }
+
+            /* Increment the counter of of numGoodValues (i.e. not clipped) */
+            *numGoodPixels = *numGoodPixels + 1;
+
+            /* Compute the sum of the "good" pixels */
+            sum = sum + image[i];
+
+            /* Compute the sum of the pixels squared */
+            sumsq = sumsq + (image[i] * image[i]);
+        }
+    }
+
+    *minValue = tmpMinValue;
+    *maxValue = tmpMaxValue;
+    *mean = (float)(sum / *numGoodPixels);
+    *stddev = (float)sqrt((sumsq - (*mean * sum)) / (*numGoodPixels - 1));
+
+    return 1;
+}
+
+static PyObject * computeMean(PyObject *obj, PyObject *args)
+{
+    PyObject *oimage;
+    PyArrayObject *image;
+    int status=0;
+    int numGoodPixels;
+    float clipmin, clipmax, mean, stddev, minValue, maxValue;
+
+    if (!PyArg_ParseTuple(args,"Off:computeMean",&oimage, &clipmin, &clipmax))
+	    return NULL;
+
+    image = (PyArrayObject *)NA_InputArray(oimage, tFloat32, C_ARRAY);
+    if (!image) return NULL;
+    
+    mean = 0;
+    stddev = 0;
+    numGoodPixels = 0;
+    minValue = 0;
+    maxValue = 0;
+
+    status = computeMean_(NA_OFFSETDATA(image), NA_elements(image), 
+			  clipmin, clipmax,
+			  &numGoodPixels, &mean, &stddev, &minValue, &maxValue);
+    Py_XDECREF(image); 
+
+    return Py_BuildValue("iffff",numGoodPixels,mean,stddev,minValue,maxValue);
+}
+
+static PyMethodDef computeMean_methods[] =
+{
+    {"computeMean",  computeMean, METH_VARARGS, 
+        "computeMean(image, clipmin, clipmax, numGoodPixels, mean, stddev, minValue, maxValue)"},
+    {0,            0}                             /* sentinel */
+};
+
+void initcomputeMean(void) {
+	Py_InitModule("computeMean", computeMean_methods);
+	import_array();
+}
+
