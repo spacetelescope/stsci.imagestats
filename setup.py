4fdb893a96fe95fe51f7d74486dc23d5fe6b2d6c
dencheva <dencheva@stsci.edu>
2005-12-20 16:55:50 +0000

    Adding license file
    git-svn-id: http://svn.stsci.edu/svn/ssb/stsci_python/stsci_python/trunk/imagestats@431 fe389314-cf27-0410-b35b-8c050e845b92

--- setup.py
+++ setup.py
@@ -1,14 +1,15 @@
 from distutils.core import setup, Extension
 import numarray
 from numarray.numarrayext import NumarrayExtension
-import sys, os.path
+import sys, os.path, string
 
 if not hasattr(sys, 'version_info') or sys.version_info < (2,2,0,'alpha',0):
     raise SystemExit, "Python 2.2 or later required to build imagestats."
 
 if numarray.__version__ < "1.1":
     raise SystemExit, "Numarray 1.1 or later required to build imagestats."
-
+ver = sys.version_info
+python_exec = 'python' + str(ver[0]) + '.' + str(ver[1])
 
 def dolocal():
     """Adds a command line option --local=<install-dir> which is an abbreviation for
@@ -19,9 +20,10 @@ def dolocal():
         print >>sys.stderr, "--local=<install-dir>    same as --install-lib=<install-dir>"
     for a in sys.argv:
         if a.startswith("--local="):
-            dir =  os.path.abspath(a.split("=")[1])
+            dir = os.path.abspath(a.split("=")[1])
             sys.argv.extend([
                 "--install-lib="+dir,
+                "--install-data="+os.path.join(dir,"imagestats")
                 ])
             sys.argv.remove(a)
 
@@ -44,8 +46,23 @@ def getExtensions(args):
                              
     return ext
 
+def getDataDir(args):
+    for a in args:
+        if string.find(a, '--home=') == 0:
+            dir = os.path.abspath(string.split(a, '=')[1])
+            data_dir = os.path.join(dir, 'lib/python/imagestats')
+        elif string.find(a, '--prefix=') == 0:
+            dir = os.path.abspath(string.split(a, '=')[1])
+            data_dir = os.path.join(dir, 'lib', python_exec, 'site-packages/imagestats')
+        elif a.startswith('--install-data='):
+            dir = os.path.abspath(string.split(a, '=')[1])
+            data_dir = dir
+        else:
+            data_dir = os.path.join(sys.prefix, 'lib', python_exec, 'site-packages/imagestats')
+    return data_dir
 
-def dosetup(ext):
+
+def dosetup(data_dir, ext):
     r = setup(name = "imagestats",
               version = "0.2.5",
               description = "Compute desired statistics values for numarray objects",
@@ -55,15 +72,18 @@ def dosetup(ext):
               platforms = ["Linux","Solaris","Mac OS X"],
               packages=['imagestats'],
               package_dir={'imagestats':'lib'},
+              data_files = [(data_dir,['lib/LICENSE.txt'])],
               ext_modules=ext)
     return r
 
 
 def main():
     args = sys.argv
+    print args
     dolocal()
+    data_dir = getDataDir(args)
     ext = getExtensions(args)
-    dosetup(ext)
+    dosetup(data_dir, ext)
 
 
 if __name__ == "__main__":
