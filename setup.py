608f57fd735255a52d04dd54c2a7b5ec73ca69be
embray <embray@stsci.edu>
2011-07-25 21:54:02 +0000

    Merged in several changes from the release branch.  People are starting to update versions in trunk, so this will help smooth things out a bit.  I'll add that I don't think any version numbers in trunk should be updated from the last release version until and unless more development is done on that project (otherwise there's no reason to increase its version number.
    git-svn-id: http://svn.stsci.edu/svn/ssb/stsci_python/stsci_python/trunk/imagestats@13397 fe389314-cf27-0410-b35b-8c050e845b92

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ import setuptools
 setuptools.command.easy_install.easy_install = easier_install
 
 setup(
-    setup_requires=['d2to1>=0.2.3', 'stsci.distutils>=0.2dev'],
+    setup_requires=['d2to1>=0.2.5', 'stsci.distutils==0.2'],
     namespace_packages=['stsci'], packages=['stsci'],
     d2to1=True,
     use_2to3=True,
