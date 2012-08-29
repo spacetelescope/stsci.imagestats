aded437ffe785537cd139c22e5e8109293d3a3ec
embray <embray@stsci.edu>
2012-08-29 22:32:22 +0000

    Set up the version.py compatibility. Bumped the version to 1.4.dev: The __version__ in the module was set to 1.3, but the version in defsetup.py was 1.1.  In doubt, went with the version in the module.  Also need to come up with a plan for dealing with these __vdate__ variables.  These could probably be handled by version.py too, by extracting the last update date from the svn info.
    git-svn-id: http://svn.stsci.edu/svn/ssb/stsci_python/stsci.imagestats/trunk@18922 fe389314-cf27-0410-b35b-8c050e845b92

--- stsci/imagestats/__init__.py
+++ stsci/imagestats/__init__.py
@@ -8,8 +8,17 @@ from histogram1d import histogram1d
 import time
 from computeMean import computeMean
 
-__version__ = '1.3'
-__vdate__ = '11-Dec-2009'
+try:
+    from .version import (__version__, __svn_revision__,
+                          __svn_full_info__, __setup_datetime__)
+except ImportError:
+    __version__ = ''
+    __svn_revision__ = ''
+    __svn_full_info__ = ''
+    __setup_datetime__ = None
+
+__vdate__ = '29-Aug-2012'
+
 
 class ImageStats:
     """ Class to compute desired statistics from array objects."""
@@ -80,11 +89,11 @@ class ImageStats:
 
             try:
                 _npix,_mean,_stddev,_min,_max = computeMean(self.image,_clipmin,_clipmax)
-                #print "_npix,_mean,_stddev,_min,_max = ",_npix,_mean,_stddev,_min,_max 
+                #print "_npix,_mean,_stddev,_min,_max = ",_npix,_mean,_stddev,_min,_max
             except:
                 raise SystemError, "An error processing the array object information occured in \
                                     the computeMean module of imagestats."
-            
+
             if _npix <= 0:
                 # Compute Global minimum and maximum
                 errormsg =  "\n##############################################\n"
@@ -97,7 +106,7 @@ class ImageStats:
                 errormsg += "  Image MIN pixel value: " + str(self.min) + '\n'
                 errormsg += "  Image MAX pixel value: " + str(self.max) + '\n\n'
                 errormsg += "# Current Clipping Range                     #\n"
-                errormsg += "       for iteration " + str(iter) + '\n' 
+                errormsg += "       for iteration " + str(iter) + '\n'
                 errormsg += "       Excluding pixel values above: " + str(_clipmax) + '\n'
                 errormsg += "       Excluding pixel values below: " + str(_clipmin) + '\n'
                 errormsg += "#                                            #\n"
@@ -111,7 +120,7 @@ class ImageStats:
                 _clipmax = _mean + self.usig * _stddev
 
         if self.fields.find('median') != -1:
-            # Use the clip range to limit the data before computing 
+            # Use the clip range to limit the data before computing
             #  the median value using numpy
             if self.nclip > 0:
                 _image = self.image[(self.image <= _clipmax) & (self.image >= _clipmin)]
@@ -124,12 +133,12 @@ class ImageStats:
         if ( (self.fields.find('mode') != -1) or (self.fields.find('midpt') != -1) ):
             # Populate the historgram
             _hwidth = self.binwidth * _stddev
-            
+
             # Special Case:  We never want the _hwidth to be smaller than the bin width.  If it is,
             # we set the hwidth to be equal to the binwidth.
             if _hwidth < self.binwidth:
                 _hwidth = self.binwidth
-            
+
             _nbins = int( (_max - _min) / _hwidth ) + 1
             _dz = float(_nbins - 1) / max(self.binwidth,float(_max - _min))
             if (_dz == 0):
