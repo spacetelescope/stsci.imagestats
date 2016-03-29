13d6795ea109d8826e1144e7403d037d44fe79fc
Joseph Hunkeler <jhunkeler@gmail.com>
2016-03-28 23:42:33 -0400

    Implement RECON

--- setup.py
+++ setup.py
@@ -1,16 +1,47 @@
 #!/usr/bin/env python
+import recon.release
+from glob import glob
+from numpy import get_include as np_include
+from setuptools import setup, find_packages, Extension
 
-try:
-    from setuptools import setup
-except ImportError:
-    from distribute_setup import use_setuptools
-    use_setuptools()
-    from setuptools import setup
+
+version = recon.release.get_info()
+recon.release.write_template(version, 'lib/stsci/imagestats')
 
 setup(
-    setup_requires=['d2to1>=0.2.5', 'stsci.distutils'],
-    namespace_packages=['stsci'], packages=['stsci'],
-    d2to1=True,
-    use_2to3=False,
-    zip_safe=False
+    name = 'stsci.imagestats',
+    version = version.pep386,
+    author = 'Warren Hack, Christopher Hanley',
+    author_email = 'help@stsci.edu',
+    description = 'Compute various useful statistical values for array objects',
+    url = 'https://github.com/spacetelescope/stsci.imagestats',
+    classifiers = [
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Scientific/Engineering :: Astronomy',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    install_requires = [
+        'nose',
+        'numpy',
+        'sphinx',
+        'stsci.sphinxext',
+    ],
+    package_dir = {
+        '':'lib',
+    },
+    packages = find_packages(),
+    package_data = {
+        '': ['LICENSE.txt'],
+    },
+    ext_modules=[
+        Extension('stsci.imagestats.buildHistogram',
+            ['src/buildHistogram.c'],
+            include_dirs=[np_include()]),
+        Extension('stsci.imagestats.computeMean',
+            ['src/computeMean.c'],
+            include_dirs=[np_include()]),
+    ],
 )
